15f9ebdcfd4cf7e3b633dc9c21d7bab87bb7ee84
Splochev <Stanislav.Plochev@gmail.com>
2025-06-12 11:01:15 +0300

    add class validator

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -11,6 +11,9 @@ export const typeOrmConfig: DataSourceOptions = {
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'card-collection-manager',
-  entities: [join(__dirname, '../modules/**/entities/*.entity{.ts,.js}')],
+  entities: [
+    join(__dirname, '../database/entities/*.entity{.ts,.js}'),
+    join(__dirname, '../modules/**/entities/*.entity{.ts,.js}'),
+  ],
   synchronize: process.env.IS_DEVELOPMENT === 'true',
 };
--- server/src/modules/users/dto/create-user.dto.ts
+++ server/src/modules/users/dto/create-user.dto.ts
@@ -1,9 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty()
+  @IsEmail()
   email?: string;
 
   @ApiProperty()
+  @IsString()
   password?: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  isEmailVerified?: boolean;
 }
