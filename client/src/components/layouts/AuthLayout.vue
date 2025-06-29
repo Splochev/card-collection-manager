5f453b20edda34714aa7e999295e0417b10ccdbb
Stanislav Plochev <stanislav@trackian.com>
2025-06-29 17:56:59 +0300

    add transition to auth pages

--- client/src/components/layouts/AuthLayout.vue
+++ client/src/components/layouts/AuthLayout.vue
@@ -6,9 +6,20 @@ import { RouterView } from 'vue-router'
 <template>
   <div class="flex h-screen">
     <div class="w-1/2 flex items-center justify-center">
-      <RouterView />
+      <Transition
+        name="fade-slide"
+        mode="out-in"
+        enter-active-class="transition-all duration-500 ease-out"
+        enter-from-class="opacity-0 translate-y-8 scale-95"
+        enter-to-class="opacity-100 translate-y-0 scale-100"
+        leave-active-class="transition-all duration-300 ease-in"
+        leave-from-class="opacity-100 translate-y-0 scale-100"
+        leave-to-class="opacity-0 -translate-y-8 scale-95"
+      >
+        <RouterView />
+      </Transition>
     </div>
-    <div class="w-1/2 flex items-center justify-center"> 
+    <div class="w-1/2 flex items-center justify-center">
       <!-- TODO add yu-gi-oh image and add cool bg color from theme -->
       <!-- <img
         alt="trackian-charts"
