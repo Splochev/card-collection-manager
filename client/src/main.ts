a9e5eb3caeafcde875a96922db276deffae685d4
Stanislav Plochev <stanislav@trackian.com>
2025-06-29 17:56:46 +0300

    enforce light theme

--- client/src/main.ts
+++ client/src/main.ts
@@ -16,10 +16,15 @@ const sdkUrl = import.meta.env.VITE_SDK_URL
 if (!sdkUrl) throw new Error('Missing VITE_SDK_URL environment variable')
 await sdkStore.initializeSdk(sdkUrl)
 
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-  },
-})
+  app.use(PrimeVue, {
+    ripple: false,
+    theme: {
+      preset: Aura,
+      options: {
+        darkMode: false,
+        darkModeSelector: ".my-app-dark",
+      },
+    },
+  });
 app.use(router)
 app.mount('#app')
