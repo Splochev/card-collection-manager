7023261a097058c12e2cc8165c25007414437761
Splochev <Stanislav.Plochev@gmail.com>
2025-10-21 07:42:50 +0300

    file restructure

--- client/src/app/(authenticated)/layout.tsx
+++ client/src/app/(authenticated)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "@/app/styles/authenticated/layout.scss";
-import TopNavigation from "../components/organisms/TopNavigation";
+import TopNavigation from "../../components/organisms/TopNavigation";
 
 export const metadata: Metadata = {
   title: "Create Next App",
